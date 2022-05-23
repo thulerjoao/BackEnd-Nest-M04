cfa7e56f7e90c42335aa99d154a7ba39cce5c710
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2022-05-23 19:50:25 -0300

    feat(game/genre): update/create error implemented

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException, UnprocessableEntityException } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateGameDto } from "./dto/create-game.dto";
 import { UpdateGameDto } from "./dto/update-game.dto";
@@ -27,7 +27,7 @@ export class GameService{
 
   create(dto: CreateGameDto): Promise<Game> {
     const data: Game = {...dto}
-    return this.prisma.game.create({data});
+    return this.prisma.game.create({data}).catch(this.handleError);
   }
 
   async update(id: string, dto: UpdateGameDto): Promise<Game> {
@@ -36,10 +36,19 @@ export class GameService{
     return this.prisma.game.update({
       where:{id},
       data,
-    })
+    }).catch(this.handleError)
   }
 
  async delete(id: string){
+    await this.findById(id);
     await this.prisma.game.delete({where:{id}})
   }
+
+  handleError(error:Error) : undefined{
+    const errorLines = error.message?.split('\n');
+    const lastLineError = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(lastLineError || 'Erro ao tentar executar')
+  }
 }
+
+
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException, UnprocessableEntityException } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateGenreDto } from "./dto/create-genre.dto";
 import { UpdateGenreDto } from "./dto/update-genre.dto";
@@ -28,7 +28,7 @@ import { Genre } from "./entities/genre-entity";
 
   create(dto: CreateGenreDto): Promise <Genre> {
     const data: Genre = {...dto}
-    return this.prisma.genre.create({data});
+    return this.prisma.genre.create({data}).catch(this.handleError);
   }
 
   async update(id: string, dto: UpdateGenreDto): Promise<Genre> {
@@ -37,10 +37,17 @@ import { Genre } from "./entities/genre-entity";
     return this.prisma.genre.update({
       where:{id},
       data,
-    })
+    }).catch(this.handleError)
   }
 
   async delete(id: string){
+    await this.findById(id);
     await this.prisma.genre.delete({where:{id}});
+  }
+
+  handleError(error:Error) : undefined{
+    const errorLines = error.message?.split('\n');
+    const lastLineError = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(lastLineError || 'Erro ao tentar executar')
   }
 }
