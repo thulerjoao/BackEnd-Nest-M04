f5b6086dd10c1cee78e813ca9150bc1dc621058c
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2022-05-23 09:20:48 -0300

    feat(prisma): module and service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { GenreModule } from './genre/genre.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [GameModule, GenreModule],
+  imports: [GameModule, GenreModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
